837d931d4c7335f898dfe28c17bb76cd4ddebb7e
Yuri Plaksyuk <yuri.plaksyuk@chestnutcorp.com>
2024-06-26 20:10:33 +0300

    implemented report generation

--- docs/report.js
+++ docs/report.js
@@ -3,45 +3,83 @@ import * as i18n from './i18n.js';
 
 const db = await open();
 
-let { year, month } = (function() {
-	const date = new Date(); date.setMonth(date.getMonth() - 1);
-	return { year: date.getFullYear(), month: date.getMonth() };
+const { lastYear, lastMonth } = (function() {
+	const date = new Date(); date.setDate(1); date.setMonth(date.getMonth() - 1);
+	return { lastYear: date.getFullYear(), lastMonth: date.getMonth() };
 })();
 
-$(function() {
+const monthDistance = ({ year, month }) => (lastYear * 12 + lastMonth) - (year * 12 + month);
+
+$(async function() {
 	const reportTemplate = $('#reportTemplate').contents();
 
 	const gridRowTemplate = $('#gridRowTemplate').contents();
-	for (let month = 0; month < 12; ++month)
-		reportTemplate.find('tbody').append(gridRowTemplate.clone());
+	for (let shift = 0; shift < 12; ++shift) {
+		let year = lastYear;
+		let month = lastMonth - shift;
 
-	db.getMembers().then((members) => {
-		members.sort((a, b) => (a.name < b.name) ? -1 : 1);
+		if (month < 0) {
+			month += 12;
+			year -= 1;
+		}
 
-		members.forEach((member) => {
-			const reportSection = reportTemplate.clone().appendTo('main');
-			reportSection.find('[name=name]').val(member.name);
-			reportSection.find('[name=birthDate]').val(member.birthDate);
-			reportSection.find('[name=baptiseDate]').val(member.baptiseDate);
+		const row = gridRowTemplate.clone();
+		row.find('td:first-child').text(i18n.formatMonthYear(month, year));
+		reportTemplate.find('tbody').prepend(row);
+	}
 
-			if (member.sex == 'M')
-				reportSection.find('[name=male]').prop('checked', true);
+	const memberReports = new Map();
 
-			if (member.sex == 'F')
-				reportSection.find('[name=female]').prop('checked', true);
+	const reports = await db.getAllReports();
+	reports.forEach((report) => {
+		const dist = monthDistance(report);
+		if (dist < 12) {
+			memberReports.has(report.memberId) || memberReports.set(report.memberId, []);
+			memberReports.get(report.memberId)[11 - dist] = report;
+		}
+	});
+	
+	const members = await db.getMembers();
 
-			if (member.congStatus == 'elder')
-				reportSection.find('[name=elder]').prop('checked', true);
+	members.sort((a, b) => (a.name < b.name) ? -1 : 1);
+	members.forEach((member) => {
+		const reportSection = reportTemplate.clone().appendTo('main');
+		reportSection.find('[name=name]').val(member.name);
+		reportSection.find('[name=birthDate]').val(member.birthDate);
+		reportSection.find('[name=baptiseDate]').val(member.baptiseDate);
 
-			if (member.congStatus == 'servant')
-				reportSection.find('[name=servant]').prop('checked', true);
+		if (member.sex == 'M')
+			reportSection.find('[name=male]').prop('checked', true);
 
-			if (member.servStatus == 'pioneer')
-				reportSection.find('[name=pioneer]').prop('checked', true);
+		if (member.sex == 'F')
+			reportSection.find('[name=female]').prop('checked', true);
 
-			if (member.servStatus == 'special')
-				reportSection.find('[name=special]').prop('checked', true);
-		});
+		if (member.congStatus == 'elder')
+			reportSection.find('[name=elder]').prop('checked', true);
 
+		if (member.congStatus == 'servant')
+			reportSection.find('[name=servant]').prop('checked', true);
+
+		if (member.servStatus == 'pioneer')
+			reportSection.find('[name=pioneer]').prop('checked', true);
+
+		if (member.servStatus == 'special')
+			reportSection.find('[name=special]').prop('checked', true);
+
+		const data = memberReports.get(member.id);
+		if (data) {
+			const rows = reportSection.find('tbody tr');
+			rows.each((index, row) => {
+				const report = data[index];
+				if (report) {
+					const cells = $(row).find('td');
+					cells[1].innerText = '+';
+					cells[2].innerText = report.studies || '';
+					cells[3].innerText = report.auxiliary ? '+' : '';
+					cells[4].innerText = report.hours || '';
+					cells[5].innerText = report.remarks || '';
+				}
+			});
+		}
 	});
 });
